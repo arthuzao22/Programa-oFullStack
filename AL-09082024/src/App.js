f3a8d31ed3c10da9858f951413c95b72f7c8b789
Gabriel <gabriel.martinho@viannasempre.com.br>
2024-08-13 21:40:38 -0300

    Falta o resto

--- AL-09082024/src/App.js
+++ AL-09082024/src/App.js
@@ -2,22 +2,74 @@ import logo from './logo.svg';
 import './App.css';
 import Evento from './components/Evento';
 import { faker } from '@faker-js/faker';
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { feeds: [], carregado: false };
+  }
 
-function App() {
-  return (
-    className
-  );
+  render() {
+    if (!this.state.carregado) {
+      this.setState({ feeds: objetos, carregado: true });
+      return (<div>Carregando...</div>);
+    }
+
+    return (
+      <div className="ui feed">
+        {
+          this.state.feeds.map(x => {
+            return (<Evento 
+              nome={x.nome} 
+              likes={x.likes} 
+              mensagem={x.mensagem} 
+              fotoPerfil={x.fotoPerfil} 
+              tempo={x.tempo} 
+            />);
+          })
+        }
+      </div>
+    );
+  }
 }
 
 const objetos = [
-  { name: "Ana", likes: 20 },
-  {name: "Bruno", likes: 15},
-  {name: "Carlos", likes: 5},
-  {name: "Daniela", likes: 45},
-  {name: "Evandro", likes: 78}
-
-]
-
+  { 
+    nome: faker.person.firstName(), 
+    likes: faker.number.int({min: 0, max: 50}), 
+    mensagem: faker.lorem.sentence(), 
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int({min: 0, max: 50})
+  },
+  { 
+    nome: faker.person.firstName(), 
+    likes: faker.number.int({min: 0, max: 50}), 
+    mensagem: faker.lorem.sentence(), 
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int({min: 0, max: 50})
+  },
+  { 
+    nome: faker.person.firstName(), 
+    likes: faker.number.int({min: 0, max: 50}), 
+    mensagem: faker.lorem.sentence(), 
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int({min: 0, max: 50})
+  },
+  { 
+    nome: faker.person.firstName(), 
+    likes: faker.number.int({min: 0, max: 50}), 
+    mensagem: faker.lorem.sentence(), 
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int({min: 0, max: 50})
+  },
+  { 
+    nome: faker.person.firstName(), 
+    likes: faker.number.int({min: 0, max: 50}), 
+    mensagem: faker.lorem.sentence(), 
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int({min: 0, max: 50})
+  }
+];
 
 export default App;
--- AL-09082024/src/components/Evento.js
+++ AL-09082024/src/components/Evento.js
@@ -1,31 +1,62 @@
 import React from "react";
-import { faker } from '@faker-js/faker';
 
+class Evento extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-const Evento = (props) => {
-    return (
-        <div className="event">
-            <div className="label">
-            <img src={faker.image.avatar()}/>
+    render() {
+        return (
+            <div className="event">
+                <div className="label">
+                    <img src={this.props.fotoPerfil} alt="..." />
+                </div>
+                <div className="content">
+                    <div className="summary">
+                        <a className="user">
+                            {this.props.nome}
+                        </a> {this.props.mensagem}
+                        <div className="date">
+                            {this.props.tempo} minutos atrás
+                        </div>
+                    </div>
+                    <div className="meta">
+                        <a className="like">
+                            <i className="like icon"></i> {this.props.likes} Likes
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="content">
-            <div className="summary">
-                <a className="user">
-                {props.name}
-                </a> added you as a friend
-                <div className="date">
-                1 Hour Ago
-                </div>
-                </div>
-                <div className="meta">
-                <a className="like">
-                    <i className="like icon"></i> {props.likes} Likes
-                </a>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 };
 
-
 export default Evento;
+
+
+// const Evento = (props) => {
+//     return (
+//         <div className="event">
+//             <div className="label">
+//                 <img src={props.fotoPerfil} alt="..." />
+//             </div>
+//             <div className="content">
+//                 <div className="summary">
+//                     <a className="user">
+//                         {this.props.nome}
+//                     </a> {props.mensagem}
+//                     <div className="date">
+//                         {props.tempo} minutos atrás
+//                     </div>
+//                 </div>
+//                 <div className="meta">
+//                     <a className="like">
+//                         <i className="like icon"></i> {props.likes} Likes
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Evento;
